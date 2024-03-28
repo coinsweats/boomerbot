077c5bb812fde617b8b6f017ebd5272adb058a33
coinsweats <162453469+coinsweats@users.noreply.github.com>
2024-03-28 12:23:30 +0000

    Create bbp.sh

---
+++ bbp.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Delete previous Bulletin
+rm -f Bulletin*.pdf
+
+# Run your Python scripts in order
+python 4chart.py
+python top100img.py
+python tpo.py
+python tpoimg.py
+
+sleep 10
+
+# Generate your PDF 
+python pdf.py
+
+# Remove PNG and text files
+rm *.png
+rm *.txt
+
+# Print the PDF Bulletin*.pdf
+# python print.py
+#!/bin/bash
+
+# The path to the file you want to print
+FILE_PATH="/Users/work/Desktop/bulletinbot/Bulletin.pdf"
+
+# Check if the file exists
+if [ -f "$FILE_PATH" ]; then
+    # Print the file
+    lpr "$FILE_PATH"
+    echo "Sent to printer: $FILE_PATH"
+    # File does not exist
+    echo "Error: File does not exist at $FILE_PATH"
+fi
+
+
