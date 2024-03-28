9d8847894910027f6b7631543fcc278c8caa4315
coinsweats <162453469+coinsweats@users.noreply.github.com>
2024-03-28 12:22:06 +0000

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -2,20 +2,20 @@
 # The is the PDF builder for printing - it collates and formats images into a 2 page bulletin
 #
 from fpdf import FPDF
+from datetime import datetime  # Import datetime module
 
 class PDF(FPDF):
     def header(self):
         # Add a header to each page if needed
         self.set_font('Arial', 'B', 12)
-        self.cell(0, 10, 'Market Bulletin', 0, 1, 'L')  # Title in the header
-
+        current_date = datetime.now().strftime("%m-%d-%Y")  # Format current date
+        self.cell(10, 10, f'Market Bulletin - {current_date}', 0, 1, 'L')  # Title with current date in the header
     
     def footer(self):
         # Add a footer to each page if needed
         self.set_y(-15)
         self.set_font('Arial', 'I', 8)
         self.cell(0, 10, ' ' + str(self.page_no()), 0, 0, 'R')  # Page number in the footer
-
 
 # Helper function to calculate x position for centered image
 def get_centered_x(image_width, page_width=210):
@@ -36,7 +36,7 @@ pdf.add_page()
 
 # Title for the first chart
 pdf.set_font("Arial", size=10)
-pdf.cell(0, 10, '90-Day Candlestick Charts', 0, 1, 'L')  # Add a title before the first chart
+pdf.cell(50, 10, '90-Day Candlestick Charts', 0, 1, 'L')  # Add a title before the first chart
 
 # Add '4chart.png' on the top half of the page
 pdf.image('4chart.png', x=5, y=32, w=190)
@@ -45,34 +45,27 @@ pdf.image('4chart.png', x=5, y=32, w=190)
 y_position = 180  # A4 page height is 297mm
 
 # Add 'top_gainers_enhanced.png' on the bottom left half of the page
-pdf.image('top_gainers_enhanced.png', x=15, y=y_position, w=195)
+pdf.image('top_gainers.png', x=20, y=y_position, w=105)
 
 # Add 'top_losers_enhanced.png' on the bottom right half of the page
-pdf.image('top_losers_enhanced.png', x=110, y=y_position, w=195)
+pdf.image('top_losers.png', x=115, y=y_position, w=105)
 
 # Second Page
 pdf.add_page()
 
 # Title for the content on the second page
 pdf.set_font("Arial", size=10) # Set the title font
-pdf.cell(0, 10, 'TPO Chart', 0, 1, 'L')  # Add the title
+pdf.cell(0, 10, 'TPO Chart - 100 ticks', 0, 1, 'L')  # Add the title
 
 # Get the width of the image for centering
 # For this example, we assume 'tpo_image_with_border.png' has a width of 190mm
 centered_x_position = get_centered_x(190)
 
 # Add 'tpo_image_with_border.png' centered on the page
-pdf.image('tpo_image_with_border.png', x=centered_x_position, y=35, w=190)
-
-
-from datetime import datetime
-
-# Get current date and time
-current_time = datetime.now()
-formatted_time = current_time.strftime("%Y-%m-%d_%H-%M")  # Format as Year-Month-Day_Hour-Minute-Second
+pdf.image('tpo_image_with_border.png', x=centered_x_position, y=35, w=180)
 
 # Save the PDF to a file with the current date and time appended
-pdf_output = f'Bullletin_{formatted_time}.pdf'
+pdf_output = f'Bulletin.pdf'
 pdf.output(pdf_output)
 
 print(f'PDF saved: {pdf_output}')
